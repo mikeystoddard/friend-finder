cb67c11e0dfa0d04c2bf1ed9d692698fd18d45a8
Michael <mikeystoddard@gmail.com>
2020-04-27 17:44:51 -0700

    initial commit

---
+++ data/friends.js
@@ -0,0 +1,89 @@
+var friendsArray = [
+    {
+        name: "Leslie",
+        photo: 
+        scores: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        name: "Ben",
+        photo: 
+        scores: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        name: "Ron",
+        photo: 
+        scores: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        name: "Donna",
+        photo: 
+        scores: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            "1",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        name: "Ann",
+        photo: 
+        scores: [
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+    }
+    
+]
+module.exports = friendsArray;
+
+
+
+
+
